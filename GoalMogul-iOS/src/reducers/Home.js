7760392a3f15358a33ba19b01f4f9fdd50de057d
Jia Zeng <jz145@duke.edu>
2018-07-10 22:01:07 -0700

    staging load goal

--- GoalMogul-iOS/src/reducers/Home.js
+++ GoalMogul-iOS/src/reducers/Home.js
@@ -6,14 +6,26 @@ import {
 
 export const HOME_MASTERMIND_OPEN_CREATE_OVERLAY = 'home_mastermind_open_create_overlay';
 export const HOME_CLOSE_CREATE_OVERLAY = 'home_mastermind_close_create_overlay';
+// Goal related constants
+export const HOME_REFRESH_GOAL = 'home_refresh_goal';
+export const HOME_REFRESH_GOAL_DONE = 'home_refresh_goal_done';
+export const HOME_LOAD_GOAL_DONE = 'home_load_goal_done';
+export const HOME_SET_GOAL_INDEX = 'home_set_goal_index'; // set current goal viewing index
 
 const INITIAL_STATE = {
   tabIndex: 0,
   mastermind: {
-    showPlus: true
+    showPlus: true,
+    goals: [],
+    limit: 20,
+    skip: 0,
+    currentIndex: 0,
+    hasNextPage: undefined
   },
   activityfeed: {
-
+    feeds: [],
+    limit: 20,
+    skip: 0
   }
 };
 
--- GoalMogul-iOS/src/redux/modules/home/mastermind/actions.js
+++ GoalMogul-iOS/src/redux/modules/home/mastermind/actions.js
@@ -1,7 +1,16 @@
 import {
   HOME_CLOSE_CREATE_OVERLAY,
-  HOME_MASTERMIND_OPEN_CREATE_OVERLAY
+  HOME_MASTERMIND_OPEN_CREATE_OVERLAY,
+  HOME_REFRESH_GOAL,
+  HOME_REFRESH_GOAL_DONE,
+  HOME_LOAD_GOAL_DONE,
+  HOME_SET_GOAL_INDEX
 } from '../../../../reducers/Home';
+
+import { api as API } from '../../../middleware/api';
+
+const DEBUG_KEY = '[ Action Home Mastermind ]';
+const BASE_ROUTE = 'secure/goal/';
 
 export const openCreateOverlay = () => ({
   type: HOME_MASTERMIND_OPEN_CREATE_OVERLAY
@@ -11,3 +20,90 @@ export const closeCreateOverlay = (tab) => ({
   type: HOME_CLOSE_CREATE_OVERLAY,
   payload: tab
 });
+
+// set currentIndex to the prev one
+export const getPrevGoal = () => (dispatch, getState) => {
+  const { currentIndex } = getState().home.mastermind;
+  if (currentIndex <= 0) {
+    return false;
+  }
+  dispatch({
+    type: HOME_SET_GOAL_INDEX,
+    payload: currentIndex - 1
+  });
+  return true;
+};
+
+// set currentIndex to the next one
+export const getNextGoal = () => (dispatch, getState) => {
+  const { currentIndex, goals } = getState().home.mastermind;
+  if (currentIndex >= goals.length) {
+    return false;
+  }
+  dispatch({
+    type: HOME_SET_GOAL_INDEX,
+    payload: currentIndex + 1
+  });
+  return true;
+};
+
+// Refresh goal for mastermind tab
+export const refreshGoals = () => (dispatch, getState) => {
+  const { token } = getState().user;
+  const { limit } = getState().home.mastermind;
+  dispatch({
+    type: HOME_REFRESH_GOAL
+  });
+  loadGoals(0, limit, token, (data) => {
+    dispatch({
+      type: HOME_REFRESH_GOAL_DONE,
+      payload: {
+        data,
+        skip: data.length,
+        limit: 20,
+        hasNextPage: !(data === undefined || data.length === 0)
+      }
+    });
+  });
+};
+
+// Load more goal for mastermind tab
+export const loadMoreGoals = () => (dispatch, getState) => {
+  const { token } = getState().user;
+  const { skip, limit } = getState().home.mastermind;
+  loadGoals(skip, limit, token, (data) => {
+    dispatch({
+      type: HOME_LOAD_GOAL_DONE,
+      payload: {
+        data,
+        skip: data.length,
+        limit: 20,
+        hasNextPage: !(data === undefined || data.length === 0)
+      }
+    });
+  });
+};
+
+/**
+ * Basic API to load goals based on skip and limit
+ * @param isRefresh:
+ * @param skip:
+ * @param limit:
+ * @param token:
+ */
+const loadGoals = (skip, limit, token, callback) => {
+  const route = '/feed';
+  API
+    .get(`${BASE_ROUTE}${route}?limit=${limit}&skip=${skip}`, token)
+    .then((res) => {
+      console.log('loading goal with res: ', res);
+      if (res) {
+        // Right now return empty data
+        callback([]);
+      }
+      console.warn(`${DEBUG_KEY}: Loading goal with no res`);
+    })
+    .catch((err) => {
+      console.log(`${DEBUG_KEY} load goal error: ${err}`);
+    });
+};
