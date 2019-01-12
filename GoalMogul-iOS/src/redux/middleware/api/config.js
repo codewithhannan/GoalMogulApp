c8c46cc65efb055b304b7200a408d726f25391ad
Jia Zeng <jz145@duke.edu>
2019-01-12 15:49:06 -0800

    Update endpoint to use the production endpoint. api.goalmogul.com

--- GoalMogul-iOS/src/redux/middleware/api/config.js
+++ GoalMogul-iOS/src/redux/middleware/api/config.js
@@ -1,4 +1,5 @@
 export const config = {
-	url: 'https://goalmogul-api-dev.herokuapp.com/api/',
-	// url: 'http://192.168.0.12:8081/api/'
+	url: 'https://api.goalmogul.com/api/' // Official endpoint
+	// url: 'https://goalmogul-api-dev.herokuapp.com/api/', # Development endpoint
+	// url: 'http://192.168.0.12:8081/api/' # Local endpoint
 };
