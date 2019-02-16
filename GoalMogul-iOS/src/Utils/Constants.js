d27414b6beb901f8c6377a5ee0c8e4c403acead6
Jia Zeng <jz145@duke.edu>
2019-02-15 21:04:04 -0800

    UI fixes for options in profile for goals and needs

--- GoalMogul-iOS/src/Utils/Constants.js
+++ GoalMogul-iOS/src/Utils/Constants.js
@@ -1,3 +1,69 @@
 export const IPHONE_MODELS_2 = ['iphone 7 plus', 'iphone x', 'iPhone xs', 'iphone xr'];
 export const IPHONE_MODELS = ['iphone 7', 'iphone 6', 
     'iphone 5', 'iphone 6 plus', 'iphone 4', 'iphone 5s'];
+
+export const SORT_BY_OPTIONS = [
+    {
+        text: 'Date Created',
+        value: 'created'
+    },
+    {
+        text: 'Last Updated',
+        value: 'updated'
+    },
+    {
+        text: 'Date Shared',
+        value: 'shared'
+    },
+    {
+        text: 'Priority',
+        value: 'Priority'
+    },
+];
+      
+export const CATEGORY_OPTIONS = [
+    {
+        text: 'All',
+        value: 'All'
+    },
+    {
+        text: 'General',
+        value: 'General'
+    },
+    {
+        text: 'Learning/Education',
+        value: 'Learning/Education'
+    },
+    {
+        text: 'Career/Business',
+        value: 'Career/Business'
+    },
+    {
+        text: 'Financial',
+        value: 'Financial'
+    },
+    {
+        text: 'Spiritual',
+        value: 'Spiritual'
+    },
+    {
+        text: 'Family/Personal',
+        value: 'Family/Personal'
+    },
+    {
+        text: 'Physical',
+        value: 'Physical'
+    },
+    {
+        text: 'Charity/Philanthropy',
+        value: 'Charity/Philanthropy'
+    },
+    {
+        text: 'Travel',
+        value: 'Travel'
+    },
+    {
+        text: 'Things',
+        value: 'Things'
+    }
+];
