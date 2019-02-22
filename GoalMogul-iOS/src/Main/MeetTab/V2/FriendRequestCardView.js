9d37a96c1d89e64a28089ae14e58cdaa5670d6ae
Jia Zeng <jz145@duke.edu>
2019-02-21 17:55:05 -0800

    Bug fixes for image display

--- GoalMogul-iOS/src/Main/MeetTab/V2/FriendRequestCardView.js
+++ GoalMogul-iOS/src/Main/MeetTab/V2/FriendRequestCardView.js
@@ -123,12 +123,14 @@ class FriendRequestCardView extends React.PureComponent {
     }
 
     renderProfileImage(item) {
+        const { user, type } = item;
+        if (!user || type === 'info') return '';
         return (
             <ProfileImage
                 imageStyle={{ height: 40, width: 40, borderRadius: 5 }}
                 defaultImageStyle={{ height: 40, width: 37, borderRadius: 5, marginLeft: 1, marginRight: 1 }}
                 imageContainerStyle={{ marginTop: 5 }}
-                imageUrl={item && item.profile ? item.profile.image : undefined}
+                imageUrl={user && user.profile ? user.profile.image : undefined}
                 imageContainerStyle={styles.imageContainerStyle}
                 userId={item._id}
             />
@@ -155,7 +157,6 @@ class FriendRequestCardView extends React.PureComponent {
     const { user, type } = item;
     if (!user || type === 'info') return '';
     const { name, profile, headline } = user;
-    // console.log(`${DEBUG_KEY}: item is: `, item);
     const detailText = headline || profile.occupation;
     return (
         <View style={{ flex: 1, marginLeft: 13 }}>
