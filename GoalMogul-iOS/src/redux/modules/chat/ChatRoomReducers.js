ec2a56ebb7cafa30e743cbe066b9c7217ff9ff8a
Jay Patel <jayp1924@gmail.com>
2019-03-28 18:52:16 -0400

    reducers are bug free and complete for chatroom

--- GoalMogul-iOS/src/redux/modules/chat/ChatRoomReducers.js
+++ GoalMogul-iOS/src/redux/modules/chat/ChatRoomReducers.js
@@ -18,6 +18,9 @@ const INITIAL_STATE = {
     selectedTab: 'chatRoomConversation',
     chatRoom: null,
     messages: [],
+    limit: 10,
+    skip: 0,
+    hasNextPage: false,
     loading: false,
     searchResults: [],
     searching: false,
@@ -28,7 +31,10 @@ export const CHAT_ROOM_LOAD_INITIAL = 'chat_room_load_initial';
 export const CHAT_ROOM_UPDATE_CHAT_ROOM = 'chat_room_update_chat_room';
 export const CHAT_ROOM_LOAD_MORE_MESSAGES_BEGIN = 'chat_room_load_more_messages_begin';
 export const CHAT_ROOM_LOAD_MORE_MESSAGES = 'chat_room_load_more_messages';
-export const CHAT_ROOM_APPEND_NEW_MESSAGES = 'chat_room_append_new_messages';
+export const CHAT_ROOM_UPDATE_MESSAGES = 'chat_room_update_messages'; // we update the entire message list when a new message comes in for consistency purposes
+export const CHAT_ROOM_SEARCH_MESSAGES_BEGIN = 'chat_room_search_messages_begin';
+export const CHAT_ROOM_SEARCH_MESSAGES_CLEAR = 'chat_room_search_messages_clear';
+export const CHAT_ROOM_SEARCH_MESSAGES = 'chat_room_search_messages';
 
 export default (state=INITIAL_STATE, action) => {
 	switch (action.type) {
@@ -45,11 +51,50 @@ export default (state=INITIAL_STATE, action) => {
             let newState = _.cloneDeep(state);
             if (messages) {
                 newState = _.set(newState, 'messages', messages);
+                newState = _.set(newState, 'hasNextPage', !!messages.length);
             };
             if (chatRoom) {
                 newState = _.set(newState, 'chatRoom', chatRoom);
             };
             return newState;
+        }
+        case CHAT_ROOM_UPDATE_CHAT_ROOM: {
+            let newState = _.cloneDeep(state);
+            return _.set(newState, 'chatRoom', action.payload);
+        }
+        case CHAT_ROOM_LOAD_MORE_MESSAGES_BEGIN: {
+            let newState = _.cloneDeep(state);
+            return _.set(newState, 'loading', true);
+        }
+        case CHAT_ROOM_LOAD_MORE_MESSAGES: {
+            let newState = _.cloneDeep(state);
+            const messages  = action.payload;
+            const oldMessages = _.get(newState, `messages`);
+            const mergedMessages = arrayUnique(oldMessages.concat(messages));
+            newState = _.set(newState, 'loading', false);
+            newState = _.set(newState, 'skip', mergedMessages.length);
+            newState = _.set(newState, 'hasNextPage', !!messages.length);
+			return _.set(newState, `messages`, mergedMessages);
+        }
+        case CHAT_ROOM_UPDATE_MESSAGES: {
+            let newState = _.cloneDeep(state);
+            const messages = action.payload;
+            newState = _.set(newState, 'skip', messages.length);
+			return _.set(newState, `messages`, messages);
+        }
+        case CHAT_ROOM_SEARCH_MESSAGES_BEGIN: {
+            let newState = _.cloneDeep(state);
+            return _.set(newState, `searching`, true);
+        }
+        case CHAT_ROOM_SEARCH_MESSAGES_CLEAR: {
+            let newState = _.cloneDeep(state);
+            newState = _.set(newState, 'searchResults', []);
+            return _.set(newState, `searching`, false);
+        }
+        case CHAT_ROOM_SEARCH_MESSAGES: {
+            let newState = _.cloneDeep(state);
+            newState = _.set(newState, 'searchResults', actions.payload);
+            return _.set(newState, `searching`, false);
         }
 		default: {
 			return { ...state };
