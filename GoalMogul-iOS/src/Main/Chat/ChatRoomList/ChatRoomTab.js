78d46dcd4584c09e6aa142f090aefa270c638bcd
Jay Patel <jayp1924@gmail.com>
2019-04-10 23:46:32 -0400

    slowed down refresh rate

--- GoalMogul-iOS/src/Main/Chat/ChatRoomList/ChatRoomTab.js
+++ GoalMogul-iOS/src/Main/Chat/ChatRoomList/ChatRoomTab.js
@@ -34,7 +34,7 @@ import { IPHONE_MODELS } from '../../../Utils/Constants';
 import { Actions } from 'react-native-router-flux';
 
 const CHATROOM_AUTO_SEARCH_DELAY_MS = 500;
-const CHATROOM_LIST_REFRESH_INTERVAL = 2000; // ms
+const CHATROOM_LIST_REFRESH_INTERVAL = 3000; // ms
 
 const SEARCHBAR_HEIGHT = Platform.OS === 'ios' &&
       IPHONE_MODELS.includes(Constants.platform.ios.model.toLowerCase())
