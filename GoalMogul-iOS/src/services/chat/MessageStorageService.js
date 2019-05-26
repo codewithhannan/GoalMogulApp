3e8afd9e67634ff9fb7270a15330158e3a6070b5
Jay Patel <jayp1924@gmail.com>
2019-05-25 20:54:42 -0400

    bug fix

--- GoalMogul-iOS/src/services/chat/MessageStorageService.js
+++ GoalMogul-iOS/src/services/chat/MessageStorageService.js
@@ -365,18 +365,16 @@ class MessageStorageService {
         localDb.insert(this._transformMessageForLocalStorage(messageDoc), (err) => {
             // if error, the message will go to the server's message queue and we can try reinserting on a later pull
             if (err) return;
-            // ack message if no error and not system message
-            if (!messageDoc.isSystemMessage) {
-                LiveChatService.emitEvent(
-                    OUTGOING_EVENT_NAMES.ackMessage,
-                    { messageAckId },
-                    (resp) => {
-                        if (resp.error) {
-                            console.log(`${DEBUG_KEY} Error ack'ing message: ${resp.message}`, messageDoc);
-                        };
-                    }
-                );
-            };
+            // ack message if no error
+            LiveChatService.emitEvent(
+                OUTGOING_EVENT_NAMES.ackMessage,
+                { messageAckId },
+                (resp) => {
+                    if (resp.error) {
+                        console.log(`${DEBUG_KEY} Error ack'ing message: ${resp.message}`, messageDoc);
+                    };
+                }
+            );
             // fire listeners to this event
             const listeners = Object.values(this.incomingMessageListeners);
             for (let listener of listeners) {
