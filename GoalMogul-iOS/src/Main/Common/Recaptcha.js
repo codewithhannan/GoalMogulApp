e6102cdc10f2aab5f6fe92210ba83a0b49c68566
a0n0d0y <jz145@duke.edu>
2019-12-30 01:16:58 -0800

    Fix further conflict

--- GoalMogul-iOS/src/Main/Common/Recaptcha.js
+++ GoalMogul-iOS/src/Main/Common/Recaptcha.js
@@ -4,11 +4,7 @@ import { WebView } from 'react-native-webview';
 import Constants from 'expo-constants';
 import Modal from 'react-native-modal';
 import { SentryRequestBuilder } from '../../monitoring/sentry';
-<<<<<<< HEAD
 import { SENTRY_TAGS, SENTRY_MESSAGE_LEVEL } from '../../monitoring/sentry/Constants';
-=======
-import { SENTRY_TAGS } from '../../monitoring/sentry/Constants';
->>>>>>> 6f031e55ff542c694224b1ab0bffa1ff6ee4fea1
 import { GOOGLE_RECAPTCHA_KEY } from '../../config';
 
 const DEBUG_KEY = '[Recaptcha]';
@@ -121,11 +117,8 @@ class Recaptcha extends Component {
                         scrollEnabled={false}
                         onLoadEnd={(e) => {
                             if (!this.state.loaded) {
-<<<<<<< HEAD
                                 // Wait for WebView to be fully loaded and converted to transparent.
                                 // Otherwise, it will be a white glitch.
-=======
->>>>>>> 6f031e55ff542c694224b1ab0bffa1ff6ee4fea1
                                 setTimeout(() => {
                                     this.setState({ loaded: true });
                                 }, 100);
