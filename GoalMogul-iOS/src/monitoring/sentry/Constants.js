6f031e55ff542c694224b1ab0bffa1ff6ee4fea1
a0n0d0y <jz145@duke.edu>
2019-12-30 01:05:41 -0800

    google recaptcha is added

---
+++ GoalMogul-iOS/src/monitoring/sentry/Constants.js
@@ -0,0 +1,14 @@
+const SENTRY_TAGS = {
+    GOOGLE_SERVIVE: 'google service',
+    ACTION: {
+        LOGIN_IN: 'login'
+    }
+};
+
+const SENTRY_TAG_VALUE = {
+    ACTIONS: {
+        FAILED: 'failed'
+    }
+};
+
+export { SENTRY_TAGS };
