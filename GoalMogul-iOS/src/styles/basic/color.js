7945e01b12cc3493c17c12a080232a18dd89370a
a0n0d0y <jz145@duke.edu>
2020-05-02 15:13:11 -0700

    Setup basic style for GM

---
+++ GoalMogul-iOS/src/styles/basic/color.js
@@ -0,0 +1,9 @@
+/**
+ * Following is the new blue for V2 
+ * 
+ * @link https://www.figma.com/file/T1ZgWm5TKDA4gtBS5gSjtc/GoalMogul-App?node-id=1%3A626
+ */
+export const GM_BLUE = "#45C9F6";
+export const GM_BLUE_LIGHT = "#9EE6FF";
+export const GM_BLUE_LIGHT_LIGHT = "#DEF7FF";
+export const GM_DOT_GRAY = "#E0E0E0";
---
+++ GoalMogul-iOS/src/styles/basic/index.js
@@ -0,0 +1,6 @@
+import text from './text';
+import color from './color';
+import button from './button';
+import card from './card';
+
+export { text, color, button, card };
