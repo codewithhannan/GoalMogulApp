1d7349e66fe71a3988ab8d3420da8cf2e768835a
a0n0d0y <jz145@duke.edu>
2020-05-02 17:21:13 -0700

    Added default container background color

--- GoalMogul-iOS/src/styles/basic/color.js
+++ GoalMogul-iOS/src/styles/basic/color.js
@@ -7,3 +7,4 @@ export const GM_BLUE = "#45C9F6";
 export const GM_BLUE_LIGHT = "#9EE6FF";
 export const GM_BLUE_LIGHT_LIGHT = "#DEF7FF";
 export const GM_DOT_GRAY = "#E0E0E0";
+export const GM_CONTAINER_BACKGROUND = "#FAFAFA";
