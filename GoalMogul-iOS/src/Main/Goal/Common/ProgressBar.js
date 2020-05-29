5c424154d823c1ce9d22adf6968c83de8f36b02e
Davinderpal Cheema <dcheema2096@gmail.com>
2020-05-29 16:28:49 -0400

    fixed dates not showing on goal detail timeline

--- GoalMogul-iOS/src/Main/Goal/Common/ProgressBar.js
+++ GoalMogul-iOS/src/Main/Goal/Common/ProgressBar.js
@@ -109,10 +109,11 @@ const ProgressBar = (props) => {
     return (
         <View style={styles.containerStyle}>
             <View style={{
-                flex: 1,
                 flexDirection: 'row',
                 justifyContent: 'space-between',
-                marginBottom: 10
+                marginBottom: 10,
+                marginLeft: 2,
+                marginRight: 2
             }}>
                 {startTimeTextView}
                 {endTimeTextView}
@@ -129,11 +130,9 @@ const ProgressBar = (props) => {
 
 const styles = {
     containerStyle: {
-        flex: 1,
         marginTop: 2
     },
     barContainerStyle: {
-        flex: 1,
         flexDirection: 'row',
         alignSelf: 'center',
         alignItems: 'center'
