adc8244eed518f33e5b1ed82b1439a9d8a2518cc
Jimmy <jimmylanbcn@gmail.com>
2020-06-20 17:01:15 -0400

    Add temp file

---
+++ GoalMogul-iOS/src/Main/Chat/Modals/CreateGroupModal.js
@@ -0,0 +1,24 @@
+/***********************************************************
+ * FILENAME: CreateGroupModal.js    TYPE: Component
+ *
+ * DESCRIPTION:
+ *      Allow user to select a photo from local directory.
+ *
+ * AUTHER: Yanxiang Lan     START DATE: 12 May 20
+ * CREDIT: Jia Zeng, for partial code written in v1.
+ ***********************************************************/
+
+import React, { Component } from "react";
+
+// Redux
+import {
+  cancelCreateOrUpdateChatroom,
+  changeModalPage,
+  createOrUpdateChatroom,
+  loadMoreFriendsSearch,
+  refreshFriendsSearch,
+  searchQueryUpdated,
+  updateSelectedMembers,
+} from "../../../redux/modules/chat/CreateChatRoomActions";
+
+class CreateGroupChat extends Component {}
