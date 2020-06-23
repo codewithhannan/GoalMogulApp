2857182cec8c36a2ef33409148b7b8bf2752b479
Jimmy <jimmylanbcn@gmail.com>
2020-06-23 14:22:18 -0400

    Update mapping

--- GoalMogul-iOS/ThemeProvider.js
+++ GoalMogul-iOS/ThemeProvider.js
@@ -20,12 +20,17 @@ import { EvaIconsPack } from "@ui-kitten/eva-icons";
  * Please consult https://akveo.github.io/react-native-ui-kitten/docs/guides/branding.
  */
 import { default as theme } from "./theme.json";
+import { default as mapping } from "./mapping.json";
 
 function ThemeProvider({ children }) {
   return (
     <>
       <IconRegistry icons={EvaIconsPack} />
-      <ApplicationProvider {...eva} theme={{ ...eva.light, ...theme }}>
+      <ApplicationProvider
+        {...eva}
+        theme={{ ...eva.light, ...theme }}
+        customMapping={mapping}
+      >
         {children}
       </ApplicationProvider>
     </>
