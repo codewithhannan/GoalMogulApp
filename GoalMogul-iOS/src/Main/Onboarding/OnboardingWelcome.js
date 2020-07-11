c41803824149aebe546a677b0dffbc4ee1bf3bb7
a0n0d0y <jz145@duke.edu>
2020-07-10 22:47:52 -0700

    Add mascot for onboarding welcome page

--- GoalMogul-iOS/src/Main/Onboarding/OnboardingWelcome.js
+++ GoalMogul-iOS/src/Main/Onboarding/OnboardingWelcome.js
@@ -1,86 +1,111 @@
-import React from "react";
-import { View, Text, Dimensions, Image } from "react-native";
-import { Actions } from "react-native-router-flux";
-import { connect } from "react-redux";
-import OnboardingHeader from "./Common/OnboardingHeader";
-import {
-  BUTTON_STYLE as buttonStyle,
-  TEXT_STYLE as textStyle,
-} from "../../styles";
-import { registrationTribeSelection } from "../../redux/modules/registration/RegistrationActions";
-import DelayedButton from "../Common/Button/DelayedButton";
-
-const screenWidth = Math.round(Dimensions.get("window").width);
 /**
  * Last page in onboarding process to welcome user to GM.
  *
- * @link https://www.figma.com/file/T1ZgWm5TKDA4gtBS5gSjtc/GoalMogul-App?node-id=24%3A195
+ * @see https://www.figma.com/file/T1ZgWm5TKDA4gtBS5gSjtc/GoalMogul-App?node-id=24%3A195
+ * @format
  */
-class OnboardingWelcome extends React.Component {
-  continue = () => {
-    const screenTransitionCallback = () => {
-      Actions.replace("drawer");
-    };
-    screenTransitionCallback();
-  };
 
-  render() {
-    return (
-      <View style={styles.containerStyle}>
-        <OnboardingHeader />
-        <View
-          style={{ flex: 1, padding: 20, marginTop: 20, alignItems: "center" }}
-        >
-          <View style={{ flex: 1, alignItems: "center", width: "100%" }}>
-            {/** Image placeholder */}
-            <View
-              style={{
-                height: screenWidth * 0.65,
-                width: screenWidth * 0.65,
-                backgroundColor: "#F2F2F2",
-              }}
-            />
-            <View style={{ marginTop: 40 }}>
-              <Text style={textStyle.onboardingTitleTextStyle}>
-                Welcome to GoalMogul!
-              </Text>
-              <Text
-                style={[
-                  textStyle.onboardingPharagraphTextStyle,
-                  { paddingLeft: 20, paddingRight: 20, marginTop: 30 },
-                ]}
-              >
-                Here you'll forget deeper & more meaningful connections, based
-                on goals & dreams that are truly important to you in life!
-              </Text>
+import React from 'react'
+import { View, Text, Dimensions, Image } from 'react-native'
+import { Actions } from 'react-native-router-flux'
+import OnboardingHeader from './Common/OnboardingHeader'
+import {
+    BUTTON_STYLE as buttonStyle,
+    TEXT_STYLE as textStyle,
+} from '../../styles'
+import DelayedButton from '../Common/Button/DelayedButton'
+import Icons from '../../asset/base64/Icons'
+
+const screenWidth = Math.round(Dimensions.get('window').width)
+class OnboardingWelcome extends React.Component {
+    continue = () => {
+        // TODO: analytics
+        const screenTransitionCallback = () => {
+            Actions.replace('drawer')
+        }
+        screenTransitionCallback()
+    }
+
+    /**
+     * Render Mascot
+     */
+    renderImage = () => {
+        return (
+            <View>
+                <Image
+                    source={Icons.LionMascotWelcome}
+                    style={{
+                        height: screenWidth * 0.7,
+                        width: screenWidth * 0.7,
+                    }}
+                />
             </View>
-          </View>
-          <DelayedButton
-            onPress={this.continue}
-            style={[
-              buttonStyle.GM_BLUE_BG_WHITE_BOLD_TEXT.containerStyle,
-              { marginBottom: 20 },
-            ]}
-          >
-            <Text style={buttonStyle.GM_BLUE_BG_WHITE_BOLD_TEXT.textStyle}>
-              Continue
-            </Text>
-          </DelayedButton>
-        </View>
-      </View>
-    );
-  }
+        )
+    }
+
+    render() {
+        return (
+            <View style={styles.containerStyle}>
+                <OnboardingHeader />
+                <View
+                    style={{
+                        flex: 1,
+                        padding: 20,
+                        marginTop: 20,
+                        alignItems: 'center',
+                    }}
+                >
+                    <View
+                        style={{ flex: 1, alignItems: 'center', width: '100%' }}
+                    >
+                        {this.renderImage()}
+                        <View style={{ marginTop: 40 }}>
+                            <Text style={textStyle.onboardingTitleTextStyle}>
+                                Welcome to GoalMogul!
+                            </Text>
+                            <Text
+                                style={[
+                                    textStyle.onboardingPharagraphTextStyle,
+                                    {
+                                        paddingLeft: 20,
+                                        paddingRight: 20,
+                                        marginTop: 30,
+                                    },
+                                ]}
+                            >
+                                Here you'll forget deeper & more meaningful
+                                connections, based on goals & dreams that are
+                                truly important to you in life!
+                            </Text>
+                        </View>
+                    </View>
+                    <DelayedButton
+                        onPress={this.continue}
+                        style={[
+                            buttonStyle.GM_BLUE_BG_WHITE_BOLD_TEXT
+                                .containerStyle,
+                            { marginBottom: 20 },
+                        ]}
+                    >
+                        <Text
+                            style={
+                                buttonStyle.GM_BLUE_BG_WHITE_BOLD_TEXT.textStyle
+                            }
+                        >
+                            Continue
+                        </Text>
+                    </DelayedButton>
+                </View>
+            </View>
+        )
+    }
 }
 
 const styles = {
-  containerStyle: {
-    flex: 1,
-    backgroundColor: "white",
-  },
-};
+    containerStyle: {
+        flex: 1,
+        backgroundColor: 'white',
+    },
+}
 
-const mapStateToProps = (state) => {
-  return {};
-};
-
-export default connect(mapStateToProps, {})(OnboardingWelcome);
+export default OnboardingWelcome
