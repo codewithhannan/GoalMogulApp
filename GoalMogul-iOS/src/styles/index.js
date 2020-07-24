45cc4d6f1bbb8db3e9c61bd7c564e7cb0176e9e9
Davinderpal Cheema <dcheema2096@gmail.com>
2020-07-24 17:36:32 -0400

    exporting letter spacings

--- GoalMogul-iOS/src/styles/index.js
+++ GoalMogul-iOS/src/styles/index.js
@@ -30,8 +30,8 @@ export const FONT_FAMILY_1 = 'SFProDisplay-Bold'
 export const FONT_FAMILY_2 = 'SFProDisplay-Regular'
 export const FONT_FAMILY_3 = 'SFProDisplay-Semibold'
 
-const LETTER_SPACING = 0.27
-const LETTER_SPACING_TITLE = 0.5
+export const LETTER_SPACING = 0.27
+export const LETTER_SPACING_TITLE = 0.5
 
 export const DEFAULT_STYLE = {
     // Headers
