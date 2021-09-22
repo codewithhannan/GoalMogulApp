b926f2f1522ba266308b12f3b15e330ae777384a
AbdulHannan1996 <johnvegas72@gmail.com>
2021-09-22 12:59:55 +0500

    before merge with hannan 22/9/2021

--- src/Main/Profile/ProfileCard/ProfileDetailCard.js
+++ src/Main/Profile/ProfileCard/ProfileDetailCard.js
@@ -34,6 +34,7 @@ import {
     openCameraRoll,
     submitUpdatingProfile,
     updateProfilePic,
+    handleTabRefresh,
 } from '../../../actions/'
 
 // Selector
@@ -180,6 +181,13 @@ class ProfileDetailCard extends Component {
         }
     }
 
+    handleRefresh = () => {
+        const { userId, pageId, selectedTab } = this.props
+        if (selectedTab === 'about') return
+        console.log(`${DEBUG_KEY}: refreshing tab`, selectedTab)
+        this.props.handleTabRefresh(selectedTab, userId, pageId)
+    }
+
     makeFriendshipStatusOptions = () => {
         return [
             {
@@ -193,6 +201,7 @@ class ProfileDetailCard extends Component {
                         'requests.incoming',
                         undefined
                     )
+                    this.handleRefresh()
                     // close bottom sheet
                     this.closeFriendRequestOptionModal()
                 },
@@ -1143,4 +1152,5 @@ export default connect(mapStateToProps, {
     openCameraRoll,
     submitUpdatingProfile,
     updateProfilePic,
+    handleTabRefresh,
 })(ProfileDetailCard)
